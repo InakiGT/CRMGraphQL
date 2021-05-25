9639c254c24bd9311bfb775dc68444feb5cc5a53
Iñaki García <yarimfuego@gmail.com>
2021-05-25 17:13:56 -0500

    Modificado un elemento del Schema

--- db/schema.js
+++ db/schema.js
@@ -46,6 +46,8 @@ const typeDefs = gql`
     type PedidoGrupo {
         id: ID
         cantidad: Int
+        nombre: String
+        precio: Float
     }
 
     type TopCliente {
@@ -87,6 +89,8 @@ const typeDefs = gql`
     input PedidoProductoInput {
         id: ID
         cantidad: Int
+        nombre: String
+        precio: Float
     }
 
     input PedidoInput {
